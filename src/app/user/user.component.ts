874f73eae5effbf4270466835e4d19bbbed20e74
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-01-25 23:39:07 +0200

    Added users data and performed for loop on them. Added search bar + button
    Remaining 3 bonus points :
    1. search ability
    2.  reset button
    3. read data from an external json file using TS.

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+
+export class UserComponent {
+
+  @Input() userData : any;
+  
+}
